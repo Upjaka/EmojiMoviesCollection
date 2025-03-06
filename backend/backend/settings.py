cb9e49a2ad999115e0b9a9e8e5de789e11eeacdd
Alexandr Ludov <alesant999@gmail.com>
2025-03-06 16:32:07 +0300

    Added users app to Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'movies_collection',
+    'users',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
