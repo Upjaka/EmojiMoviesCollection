47312a8f5869b4749f61f8bf7ecf11e4e876e66f
Alexandr Ludov <alesant999@gmail.com>
2025-03-06 19:38:14 +0300

    Add custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+
+    GENRE_CHOICES = [
+        ('action', 'Боевик'),
+        ('comedy', 'Комедия'),
+        ('drama', 'Драма'),
+        ('fantasy', 'Фэнтези'),
+        ('horror', 'Ужасы'),
+        ('romance', 'Романтика'),
+        ('sci-fi', 'Научная фантастика'),
+        ('thriller', 'Триллер'),
+        ('animation', 'Анимация'),
+    ]
+
+    age = models.PositiveIntegerField(null=True, blank=True, verbose_name="Возраст")
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name="Аватар")
+    favorite_genres = models.CharField(max_length=50, choices=GENRE_CHOICES, null=True, blank=True, verbose_name="Любимые жанры")
+
+    def __str__(self):
+        return self.username
