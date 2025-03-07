67e213bc154b7019004ee49cbf8fb093be689f20
Uplaka <ludov.ai@mail.ru>
2025-03-07 16:56:14 +0300

    Add main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,26 +5,52 @@ import './App.css'
 import MoviesList from './components/MovieList'
 
 function App() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Функция для обработки отправки формы
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Простая валидация формы
+    if (!email || !password) {
+      setError('Пожалуйста, заполните все поля');
+      return;
+    }
+
+    // Здесь можно обработать отправку данных (например, API)
+    console.log('Вход с почтой:', email, 'и паролем:', password);
+
+    // После успешной отправки закрываем модальное окно
+    closeModal();
+  };
+
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header className="header">
+          <div className="logo">Фильмовый Портал</div>
+          <button className="login-btn" onClick={openModal}>Вход</button>
+        </header>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <h1>Список фильмов</h1>
         <MoviesList />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
