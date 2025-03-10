712924089614fa7af934234d4e0bfe09d3637f3a
Uplaka <ludov.ai@mail.ru>
2025-03-10 16:42:49 +0300

    Add reactions to movie card

--- frontend/src/components/MovieListItem.jsx
+++ frontend/src/components/MovieListItem.jsx
@@ -1,24 +1,54 @@
 import React from "react";
-import "./MovieListItem.css"
+import "./MovieListItem.css";
 
-const MovieListItem = ({ title, year, genres, poster }) => {
-    const genreList = genres.split(',').slice(0, 3);
-  
-    return (
-      <div className="movie-card">
-        <img src={poster} alt={title} className="movie-poster" />
-        <div className="movie-details">
-          <div className="movie-title-container">
-            <span className="movie-title">{title}</span>
-            <span className="movie-year">{year}</span>
-          </div>
-          <div className="movie-genres">
-            {genreList.join(", ")}
+const MovieListItem = ({ title, year, genres, poster, reactions_count }) => {
+  const genreList = genres.split(',').slice(0, 3);
+
+  const emojiMap = {
+    like: '👍',
+    funny: '🤣',
+    love: '😍',
+    sad: '😢',
+    shocked: '😱',
+    mindblown: '🤯',
+    respect: '🫵',
+    dislike: '👎',
+    clown: '🤡',
+    poop: '💩',
+    heart: '❤️',
+    thinking: '🤔',
+    angry: '😡',
+    fire: '🔥',
+    ghost: '👻'
+  };
+
+  const topReactions = Object.entries(reactions_count).slice(0, 4);
+
+  return (
+    <div className="movie-card">
+      <img src={poster} alt={title} className="movie-poster" />
+      <div className="movie-details">
+        <div className="movie-title-container">
+          <span className="movie-title">{title}</span>
+          <span className="movie-year">{year}</span>
+        </div>
+        <div className="movie-genres">
+          {genreList.join(", ")}
+        </div>
+
+        <div className="movie-reactions">
+          <div className="reactions-container">
+            {topReactions.map(([reaction, count]) => (
+              <div key={reaction} className="reaction-item">
+                <span className="reaction-icon">{emojiMap[reaction]}</span>
+                <span className="reaction-count">{count}</span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 export default MovieListItem;
