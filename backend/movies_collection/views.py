ab404b6ee725ac5c3eb3e2cc3cdf0732c7af2208
Uplaka <ludov.ai@mail.ru>
2025-03-10 16:31:50 +0300

    Add sort for reactions

--- backend/movies_collection/views.py
+++ backend/movies_collection/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Movie, Reaction
 from .serializers import MovieSerializer
 
+
 @api_view(['GET'])
 def movie_list(request):
     movies = Movie.objects.all()
@@ -27,7 +28,35 @@ def movie_list(request):
         ghost_count=Count('reactions', filter=Q(reactions__reaction='ghost'))
     )
 
-    # Сериализуем фильмы
-    serializer = MovieSerializer(movies_with_reactions, many=True)
+    # Формируем список реакций для каждого фильма
+    movie_data = []
+    for movie in movies_with_reactions:
+        reactions_dict = {
+            'like': movie.like_count,
+            'funny': movie.funny_count,
+            'love': movie.love_count,
+            'sad': movie.sad_count,
+            'shocked': movie.shocked_count,
+            'mindblown': movie.mindblown_count,
+            'respect': movie.respect_count,
+            'dislike': movie.dislike_count,
+            'clown': movie.clown_count,
+            'poop': movie.poop_count,
+            'heart': movie.heart_count,
+            'thinking': movie.thinking_count,
+            'angry': movie.angry_count,
+            'fire': movie.fire_count,
+            'ghost': movie.ghost_count
+        }
 
-    return Response(serializer.data)
+        # Сортируем реакции по количеству в убывающем порядке
+        sorted_reactions = dict(sorted(reactions_dict.items(), key=lambda item: item[1], reverse=True))
+
+        # Сериализуем фильм с добавлением отсортированных реакций
+        movie_serializer = MovieSerializer(movie)
+        movie_data.append({
+            **movie_serializer.data,
+            'reactions_count': sorted_reactions
+        })
+
+    return Response(movie_data)
