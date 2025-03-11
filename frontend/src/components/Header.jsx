d3b7f87c8b1455775cd86f77035aca14cb86a04d
Uplaka <ludov.ai@mail.ru>
2025-03-11 18:41:52 +0300

    Add modal window for successful registration

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import "../styles/Header.css";
 import AuthModal from "./AuthModal";
-import axiosInstance from "../axiosInstance";
+import { useDispatch, useSelector } from "react-redux";
+import { login, register } from "../store/authSlice"; // Импортируем асинхронные операции
 
 const Header = () => {
   const logoSrc = "/logo.png";
+  const dispatch = useDispatch();
+  const { isAuthenticated, error } = useSelector((state) => state.auth); // Извлекаем данные из Redux
 
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [error, setError] = useState("");
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isRegistering, setIsRegistering] = useState(false);
 
   const openModal = () => {
@@ -20,33 +18,31 @@ const Header = () => {
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setError("");
   };
 
   const handleLogin = async (username, password, setError) => {
     try {
-      const { data } = await axiosInstance.post("/token/", { username, password });
-
-      localStorage.setItem("access", data.access);
-      localStorage.setItem("refresh", data.refresh);
-
-      setIsAuthenticated(true);
-      setError("");
-      closeModal();
+      const action = await dispatch(login({ username, password }));
+      if (action.error) {
+        setError(action.error.message);
+      } else {
+        closeModal();
+      }
     } catch (err) {
-      setError(err.response?.data?.message || "Ошибка входа");
+      setError("Ошибка входа");
     }
   };
 
   const handleRegister = async (username, password, setError) => {
     try {
-      const { data } = await axiosInstance.post("/register/", { username, password });
-
-      console.log("Успешная регистрация:", data);
-      setIsRegistering(false);
-      setError(null);
+      const action = await dispatch(register({ username, password }));
+      if (action.error) {
+        setError(action.error.message);
+      } else {
+        setIsRegistering(false); // Сменить на экран входа
+      }
     } catch (err) {
-      setError(err.response?.data?.detail || "Ошибка регистрации");
+      setError("Ошибка регистрации");
     }
   };
 
@@ -54,17 +50,11 @@ const Header = () => {
     <header className="py-4">
       <div className="container px-4 px-lg-5 d-flex justify-content-between align-items-center">
         <div className="d-flex align-items-center">
-        <img 
-          src={logoSrc}
-          alt="Logo" 
-          className="img-logo me-2"
-        />
-        <h3 className="display-4 fw-bolder text-title m-0">Emoji Movies</h3>
-      </div>
+          <img src={logoSrc} alt="Logo" className="img-logo me-2" />
+          <h3 className="display-4 fw-bolder text-title m-0">Emoji Movies</h3>
+        </div>
         <button className="btn-login" onClick={openModal}>
-          <span>
-            Вход
-          </span>
+          <span>Вход</span>
         </button>
       </div>
 
@@ -76,6 +66,7 @@ const Header = () => {
         setIsRegistering={setIsRegistering}
         handleLogin={handleLogin}
         handleRegister={handleRegister}
+        error={error} // Передаем ошибку из Redux
       />
     </header>
   );
