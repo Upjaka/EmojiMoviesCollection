daeba834c911e744bcde6648f258b02b7b1eaa27
Uplaka <ludov.ai@mail.ru>
2025-03-11 18:41:29 +0300

    Add Redux for authentication

---
+++ frontend/src/store/authSlice.js
@@ -0,0 +1,71 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../axiosInstance";
+
+// Асинхронный action для логина
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.post("/token/", { username, password });
+      return response.data; // Успешный ответ
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || "Ошибка входа");
+    }
+  }
+);
+
+// Асинхронный action для регистрации
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.post("/register/", { username, password });
+      return response.data; // Успешный ответ
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.detail || "Ошибка регистрации");
+    }
+  }
+);
+
+const initialState = {
+  isAuthenticated: false,
+  isRegistered: false,
+  error: null,
+  accessToken: null,
+  refreshToken: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout(state) {
+      state.isAuthenticated = false;
+      state.accessToken = null;
+      state.refreshToken = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        state.isAuthenticated = true;
+        state.accessToken = action.payload.access;
+        state.refreshToken = action.payload.refresh;
+        state.error = null;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(register.fulfilled, (state) => {
+        state.isRegistered = true;
+        state.error = null;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+
+export default authSlice.reducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./moviesSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    auth: authReducer
   },
 });
 
