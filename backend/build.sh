9fcd5a33a6d114ff1fa1e946be6a2faa8ba656f6
Uplaka <ludov.ai@mail.ru>
2025-03-27 15:26:10 +0300

    Update logs in build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+set -e  # Stop the script if any command fails
+
 pip install -r requirements.txt
 
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput | tee collectstatic.log
 
 echo "Running makemigrations..."
-python manage.py makemigrations 2>&1 | tee /dev/stderr
+python manage.py makemigrations --verbosity 2 | tee /dev/stderr
 
 echo "Running migrate..."
-python manage.py migrate 2>&1 | tee /dev/stderr
+python manage.py migrate --verbosity 2 | tee /dev/stderr
 
 echo "Build script completed!"
-
