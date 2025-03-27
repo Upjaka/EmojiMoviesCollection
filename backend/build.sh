d2c07e4d2ada56f15210d351f389d32cfb352231
Uplaka <ludov.ai@mail.ru>
2025-03-27 15:22:37 +0300

    Add logs to build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -6,10 +6,10 @@ echo "Running collectstatic..."
 python manage.py collectstatic --noinput | tee collectstatic.log
 
 echo "Running makemigrations..."
-python manage.py makemigrations | tee makemigrations.log
+python manage.py makemigrations 2>&1 | tee /dev/stderr
 
 echo "Running migrate..."
-python manage.py migrate | tee migrate.log
+python manage.py migrate 2>&1 | tee /dev/stderr
 
 echo "Build script completed!"
 
